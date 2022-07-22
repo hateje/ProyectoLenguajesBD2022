228dbb9d83eccb453d91a184deec9065c9199b67
machoquiros <machoquiros@gmail.com>
2022-07-21 20:52:31 -0700

    consulta sql de la base de datos
    Se imprime una consulta de SQL en una tabla en PHP
    
    Co-Authored-By: luismurillo12 <98240656+luismurillo12@users.noreply.github.com>
    Co-Authored-By: Michael Blanco <94593474+MikeBlanco09@users.noreply.github.com>
    Co-Authored-By: hateje <109327747+hateje@users.noreply.github.com>

--- index.php
+++ index.php
@@ -3,12 +3,34 @@
 <?php  
 // crear conexion con oracle
 $conexion = oci_connect("adminProyecto", "123", "localhost/orcl"); 
- 
+
+
+
+
+
 if (!$conexion) {    
   $m = oci_error();    
   echo $m['message'], "n";    
   exit; 
 } else {    
-  echo "Conexión con éxito a Oracle!"; } 
+  echo "Conexión con éxito a Oracle!"; 
+  // Obtener los resultados de la consulta
+  $test = oci_parse($conexion, 'select * from usuario where usuarioid = 1');
+
+    oci_execute($test);
+
+    print "<table border='1'>\n";
+    while ($fila = oci_fetch_array($test, OCI_ASSOC+OCI_RETURN_NULLS)) {
+        print "<tr>\n";
+        foreach ($fila as $elemento) {
+            print "    <td>" . ($elemento !== null ? htmlentities($elemento, ENT_QUOTES) : "") . "</td>\n";
+        }
+        print "</tr>\n";
+    }
+    print "</table>\n";
+
+    oci_free_statement($test);
+    oci_close($conexion);
+} 
  
 ?>
