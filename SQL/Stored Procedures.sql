9785cbe1d4ee520ab0b379233a5f15edc288d1b8
Michael Quiros <machoquiros@gmail.com>
2022-08-20 21:04:51 -0600

    SP getTipoVehiculo
    Co-Authored-By: hateje <109327747+hateje@users.noreply.github.com>
    Co-Authored-By: Michael Blanco <94593474+MikeBlanco09@users.noreply.github.com>
    Co-Authored-By: luismurillo12 <98240656+luismurillo12@users.noreply.github.com>

--- SQL/Stored Procedures.sql
+++ SQL/Stored Procedures.sql
@@ -5,3 +5,11 @@ Create or replace procedure getAllUsers (mi_cursor out SYS_REFCURSOR)
         open mi_cursor for
             select * from usuario;
     end;
+
+--Obtener todos los usuarios
+Create or replace procedure getTipoVehiculo (mi_cursor out SYS_REFCURSOR)
+ as
+    begin
+        open mi_cursor for
+            select * from tipo_vehiculo;
+    end;
